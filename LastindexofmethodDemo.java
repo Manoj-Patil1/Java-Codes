ffef1aea4b04098aed232dd40ea8b43fd3f09406
Manoj-Patil1 <manojpatil17602@gmail.com>
2024-02-09 23:01:35 +0530

    [ADDED]: Manoj - Java Program for Check LastIndex

---
+++ LastindexofmethodDemo.java
@@ -0,0 +1,9 @@
+package com.string;
+
+public class LastindexofmethodDemo {
+    public static void main(String[] args) {
+        String s1 = "Welcome to my Class :";
+        System.out.println("Last index of "+s1.lastIndexOf('C'));
+        System.out.println("Last index of "+s1.lastIndexOf('C',12));
+    }
+}
