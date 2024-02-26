7f4cbe893a5188be9ced2d6941ed7da73253464f
Manoj-Patil1 <manojpatil17602@gmail.com>
2024-02-26 17:14:07 +0530

    [ADDED]: Mnaoj - Java Program for Static Variable

---
+++ Variable2.java
@@ -0,0 +1,10 @@
+package com.codegalaxy;
+
+public class Variable2 {
+    static int i = 10;
+    public static void main(String[] args) {
+        System.out.println("Static Variable : "+i);
+        i = 20;
+        System.out.println("Modify Static Variable : "+i);
+    }
+}
