6d08ae79680d49579aebfb08b803a3aaab20b481
Manoj-Patil1 <manojpatil17602@gmail.com>
2024-02-26 17:12:20 +0530

    [ADDED]: Manoj - Java Program for Local Variable

---
+++ Variable.java
@@ -0,0 +1,13 @@
+package com.codegalaxy;
+
+public class Variable {
+    void dis(){
+        int a = 10;
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        Variable v = new Variable();
+        v.dis();
+    }
+
+}
